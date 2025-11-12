99179818b1c92699f8bb24dc4cdcaeecf54edf1d
Jared RO <jjared.ro@gmail.com>
2025-11-11 21:38:56 -0600

    Se crea la función de asignación de personas para secret santa, también se configura un __name__=="__main__" que se ejecutara unicamente si ejecutamos el código en la terminal

---
+++ logic.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import numpy as np
+import random
+import venv
+
+def asignacion_personas(personas):
+    # Diccionario para almacenar las asignaciones
+    asignacion = {}
+
+    # Convertir las claves del diccionario a una lista y mezclarla
+    nombres_rand = list(personas.keys())
+    random.shuffle(nombres_rand)
+
+    # Asignar a cada persona otra persona aleatoria
+    for persona_a in personas.keys():
+        n=len(nombres_rand)
+        persona_b = nombres_rand[n-1]
+        while (persona_b == persona_a):
+            random.shuffle(nombres_rand)
+            persona_b = nombres_rand[n-1]
+        asignacion[persona_a] = persona_b
+        nombres_rand.remove(persona_b)
+        print(f'A {persona_a} le toca {persona_b}')
+    
+if __name__ == "__main__":
+    #esto solo se ejecutará cuando llame la función desde la terminal
+    personas = {
+        'Josué Rivera': 'jjared.ro@gmail.com',
+        'Iván Rivera': 'asdfasdfgasdfgh4@gmail.com',
+        'Rafael Rivera': 'rafaelriveralopez@gmail.com',
+        'Diego Rivera': 'diero.rivera@ciencias.com'
+    }
+    asignacion_personas(personas)
