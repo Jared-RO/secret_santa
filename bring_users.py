28c0f7f97fc2f5540b477ffe6c8edccea1e33f57
Jared RO <jjared.ro@gmail.com>
2025-11-13 20:12:37 -0600

    correccion de estilo en nombres

--- bring_users.py
+++ bring_users.py
@@ -6,3 +6,6 @@ def input_users():
     df = pd.read_csv('./users_file/users.csv', header=0)
     personas = pd.Series(df.Correo.values, index=df.Nombre_Usuario).to_dict()
     return personas
+
+if __name__ == "__main__":
+    print(input_users())  
--- logic_asignment.py
+++ logic_asignment.py
@@ -20,7 +20,8 @@ def asignment(personas):
             persona_b = nombres_rand[n-1]
         asignacion[persona_a] = persona_b
         nombres_rand.remove(persona_b)
-        print(f'A {persona_a} le toca {persona_b}')
+        # print(f'A {persona_a} le toca {persona_b}')
+    return asignacion
     
 if __name__ == "__main__":
     #esto solo se ejecutará cuando llame la función desde la terminal
@@ -30,4 +31,4 @@ if __name__ == "__main__":
         'Rafael Rivera': 'rafaelriveralopez@gmail.com',
         'Diego Rivera': 'diero.rivera@ciencias.com'
     }
-    asignacion_personas(personas)
+    print(asignment(personas))
