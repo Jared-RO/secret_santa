978d0b3556fc095894926f5b5a168f47b389549f
Jared RO <jjared.ro@gmail.com>
2025-12-04 21:09:30 -0600

    alistamos black automatico en el repo

--- api.py
+++ api.py
@@ -3,7 +3,11 @@ from core import run_secret_santa
 
 app = FastAPI()
 
+
 @app.get("/run")
 def run():
     result = run_secret_santa()
-    return {"status": "ok", "assignments": result}
+    return {
+        "status": "ok",
+        "assignments": result,
+    }
